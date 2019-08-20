c40c62a14c76bd96fa4ac78a2262da6af62b74f6
Islam Sayed <islam.sayed8@gmail.com>
2019-08-20 12:36:30 +0200

    feat: add textarea, handleChange()

--- src/components/SummaryInput.js
+++ src/components/SummaryInput.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 class SummaryInput extends Component {
     state = {  }
+
+    handleChange = e => this.props.updateSummary(e.target.value)  
+
     render() { 
         return ( 
-            <div></div>
+            <div>
+                <textarea id="summary-input" name="summary" onChange={this.handleChange} placeholder="Summary" required />
+            </div>
          );
     }
 }
