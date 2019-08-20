50d6120c0be7b7bc574c101bc01156b53cda6186
Islam Sayed <islam.sayed8@gmail.com>
2019-08-20 12:32:30 +0200

    feat: func addTitle, addLink, addSummary, handleClick

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,13 +3,37 @@ import TitleInput from './TitleInput'
 import LinkInput from './LinkInput'
 import SummaryInput from './SummaryInput'
 class Form extends Component {
-    state = {  }
+    state = { 
+        title: "",
+        link: "",
+        summary: ""
+     }
+
+    addTitle = (title) => this.setState({
+        title,
+    })
+    addLink = (link) => this.setState({
+        link,
+    })
+    addSummary = (summary) => this.setState({
+        summary,
+    })
+
+    handleClick = e => {
+        e.preventDefault()
+        const title = this.state.title
+        const link = this.state.link
+        const summary = this.state.summary
+        this.props.addItem(title, link, summary)
+    }
     render() { 
+        
         return ( 
             <form>
-                <TitleInput />
-                <LinkInput />
-                <SummaryInput />
+                <TitleInput updateTitle={this.addTitle} />
+                <LinkInput updateLink={this.addLink} />
+                <SummaryInput updateSummary={this.addSummary} />
+                <button id="add-btn" onClick={this.handleClick}>ADD</button>
             </form>
          );
     }
