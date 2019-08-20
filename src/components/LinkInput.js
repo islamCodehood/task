34b7665b9eca112b849684c4af7f643f4fb9f9e0
Islam Sayed <islam.sayed8@gmail.com>
2019-08-20 18:39:22 +0200

    remove pattern attr, add class for styling

--- src/components/LinkInput.js
+++ src/components/LinkInput.js
@@ -3,12 +3,19 @@ import React, { Component } from "react";
 class LinkInput extends Component {
   state = {};
 
-  handleChange = e => this.props.updateLink(e.target.value)
+  handleChange = e => this.props.updateLink(e.target.value);
 
   render() {
     return (
-      <div>
-        <input type="url" id="link-input" onChange={this.handleChange} placeholder="Link" required />
+      <div className="input-wrapper">
+        <input
+          type="url"
+          id="link-input"
+          className="input-style"
+          onChange={this.handleChange}
+          placeholder="Link"
+          required
+        />
       </div>
     );
   }
