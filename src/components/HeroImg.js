9b6ee9f9eb272ce25c672c8efe816675eca4f8d4
Islam Sayed <islam.sayed8@gmail.com>
2019-08-20 18:38:54 +0200

    change in wrappers for flexbox

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Logo from './Logo'
 import NavBar from './NavBar'
+import BurgerIcon from './BurgerIcon'
+import SideNav from './SideNav'
+import Carousel from './Carousel'
 
 const HeroImg = props => {
     return ( 
-        <div>
-            <Logo />
-            <NavBar />
+        <div className="hero-img">
+            <BurgerIcon />
+            <SideNav />
+            <div className="nav-wrapper">
+                <Logo />
+                <NavBar />
+            </div>
+            <Carousel />
         </div>
      );
 }
